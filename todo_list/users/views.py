80506acaba274c082097019649740a615a171393
Muhi2007 <muhammadteymurlu07@gmail.com>
2026-01-07 22:28:23 +0400

    Initial commit

---
+++ todo_list/users/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account is created for {username}')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+        
+    return render(request, template_name='users/signup.html', context={"form":form})
