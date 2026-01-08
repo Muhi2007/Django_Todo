9f801ea891ea4acaa29c096991035fc5630d8420
Muhi2007 <muhammadteymurlu07@gmail.com>
2026-01-08 23:26:38 +0400

    Profile module

--- todo_list/users/forms.py
+++ todo_list/users/forms.py
@@ -4,8 +4,10 @@ from django import forms
 
 class UserSigninForm(UserCreationForm):
     email = forms.EmailField()
+    first_name = forms.CharField(max_length=150)
+    last_name = forms.CharField(max_length=150)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
